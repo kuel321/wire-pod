2beeff66a06529c4c259f1c944e76a39b0cdd97a
kuel321 <lukeshort321@gmail.com>
2023-12-09 01:08:16 -0500

    test

--- chipper/pkg/wirepod/preqs/intent_graph.go
+++ chipper/pkg/wirepod/preqs/intent_graph.go
@@ -7,7 +7,7 @@ import (
 	"github.com/kercre123/wire-pod/chipper/pkg/vtt"
 	sr "github.com/kercre123/wire-pod/chipper/pkg/wirepod/speechrequest"
 	ttr "github.com/kercre123/wire-pod/chipper/pkg/wirepod/ttr"
-	sdkapp "github.com/kercre123/vector-go-sdk/pkg/wirepod/sdkapp"
+
 
 )
 
@@ -41,7 +41,7 @@ func (s *Server) ProcessIntentGraph(req *vtt.IntentGraphRequest) (*vtt.IntentGra
 	if !successMatched {
 		logger.Println("No intent was matched.")
        
-		sdk.SayText()
+		
 		/*
 		if vars.APIConfig.Knowledge.Enable && vars.APIConfig.Knowledge.Provider == "openai" && len([]rune(transcribedText)) >= 8 {
 			apiResponse := openaiRequest(transcribedText)
