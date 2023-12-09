c8b74031722f368b7be688dbef3cdc8e7bdeac53
kuel321 <lukeshort321@gmail.com>
2023-12-08 21:40:41 -0500

    test

--- chipper/pkg/wirepod/preqs/intent_graph.go
+++ chipper/pkg/wirepod/preqs/intent_graph.go
@@ -1,7 +1,7 @@
 package processreqs
 
 import (
-	//pb "github.com/digital-dream-labs/api/go/chipperpb"
+	pb "github.com/digital-dream-labs/api/go/chipperpb"
 	"github.com/kercre123/wire-pod/chipper/pkg/logger"
 	"github.com/kercre123/wire-pod/chipper/pkg/vars"
 	"github.com/kercre123/wire-pod/chipper/pkg/vtt"
@@ -38,7 +38,7 @@ func (s *Server) ProcessIntentGraph(req *vtt.IntentGraphRequest) (*vtt.IntentGra
 	}
 	if !successMatched {
 		logger.Println("No intent was matched.")
-		/*
+		
 		if vars.APIConfig.Knowledge.Enable && vars.APIConfig.Knowledge.Provider == "openai" && len([]rune(transcribedText)) >= 8 {
 			apiResponse := openaiRequest(transcribedText)
 			response := &pb.IntentGraphResponse{
@@ -49,12 +49,12 @@ func (s *Server) ProcessIntentGraph(req *vtt.IntentGraphRequest) (*vtt.IntentGra
 				QueryText:    transcribedText,
 				IsFinal:      true,
 			}
-			
-			req.Stream.Send(response)
+			logger.Println(response)
+			//req.Stream.Send(response)
 			
 			return nil, nil
 		}
-		*/
+		
 		ttr.IntentPass(req, "intent_system_noaudio", transcribedText, map[string]string{"": ""}, false)
 		logger.Println(transcribedText + "testing this logger out")
 		return nil, nil
