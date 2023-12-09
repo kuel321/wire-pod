1e8222f88c238e577f909c247eb358198abd6ccb
kuel321 <lukeshort321@gmail.com>
2023-12-08 21:35:24 -0500

    test

--- chipper/pkg/wirepod/preqs/intent_graph.go
+++ chipper/pkg/wirepod/preqs/intent_graph.go
@@ -38,6 +38,7 @@ func (s *Server) ProcessIntentGraph(req *vtt.IntentGraphRequest) (*vtt.IntentGra
 	}
 	if !successMatched {
 		logger.Println("No intent was matched.")
+		/*
 		if vars.APIConfig.Knowledge.Enable && vars.APIConfig.Knowledge.Provider == "openai" && len([]rune(transcribedText)) >= 8 {
 			apiResponse := openaiRequest(transcribedText)
 			response := &pb.IntentGraphResponse{
@@ -48,7 +49,7 @@ func (s *Server) ProcessIntentGraph(req *vtt.IntentGraphRequest) (*vtt.IntentGra
 				QueryText:    transcribedText,
 				IsFinal:      true,
 			}
-			
+			*/
 			//req.Stream.Send(response)
 			return nil, nil
 		}
