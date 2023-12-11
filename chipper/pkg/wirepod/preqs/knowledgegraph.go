47e861cb0852cf49b31782e808c6412dc38d2809
kuel321 <lukeshort321@gmail.com>
2023-12-10 20:36:21 -0500

    test

--- chipper/pkg/wirepod/preqs/knowledgegraph.go
+++ chipper/pkg/wirepod/preqs/knowledgegraph.go
@@ -117,7 +117,7 @@ func openaiRequest(transcribedText string) string {
 	formData := `{
 		"model": "gpt-3.5-turbo-instruct",
 		"prompt": "` + sendString + `",
-		"temperature": 2,
+		"temperature": 0.7,
 		"max_tokens": 256,
 		"top_p": 1,
 		"frequency_penalty": 0.2,
