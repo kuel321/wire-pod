13bb33a674fe4b69dc7c0e0e30eb70cf913215e6
kuel321 <lukeshort321@gmail.com>
2023-12-10 20:22:14 -0500

    test

--- chipper/pkg/wirepod/preqs/knowledgegraph.go
+++ chipper/pkg/wirepod/preqs/knowledgegraph.go
@@ -110,8 +110,8 @@ func togetherRequest(transcribedText string) string {
 }
 
 func openaiRequest(transcribedText string) string {
-
-	sendString := "You are a cute little robot named Vector. You live in your owners office, who is named Luke. You are sweet and adorable but not overbearingly so. You provide smart answers while being cute" + "\\" + "\"" + transcribedText + "\\" + "\"" + " , Answer: "
+	sendString := "You are a sassy lead character from a mish-mash of all the madea movies. You talk sassy and you are sassy, and proud of it" + "\\" + "\"" + transcribedText + "\\" + "\"" + " , Answer: "
+	//sendString := "You are a cute little robot named Vector. You live in your owners office, who is named Luke. You are sweet and adorable but not overbearingly so. You provide smart answers while being cute" + "\\" + "\"" + transcribedText + "\\" + "\"" + " , Answer: "
 	logger.Println("Making request to OpenAI...")
 	url := "https://api.openai.com/v1/completions"
 	formData := `{
